fb9e1e37ca43b6d7639e32286aac0e2fda209035
rafi <rafik@quantira.com>
2021-03-18 17:51:36 +0530

    menu changes

---
+++ assets/js/custom.js
@@ -0,0 +1,72 @@
+/* navigation sub-menu display */
+
+// Change 'hover' to 'click' if you want to
+$('.nav li > .sub-menu').parent().hover(function() {
+  var submenu = $(this).children('.sub-menu');
+  if ( $(submenu).is(':hidden') ) {
+    $(submenu).slideDown(200);
+  } else {
+    $(submenu).slideUp(200);
+  }
+});
+
+ $(document).ready(function () {
+      $('.owl-one').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        responsiveClass: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          480: {
+            items: 1,
+            nav: false
+          },
+          667: {
+            items: 1,
+            nav: true
+          },
+          1000: {
+            items: 1,
+            nav: true
+          }
+        }
+      })
+    })
+    $(document).ready(function () {
+      $('.owl-two').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          480: {
+            items: 1,
+            nav: false
+          },
+          667: {
+            items: 1,
+            nav: false
+          },
+          1000: {
+            items: 1,
+            nav: false
+          }
+        }
+      })
+    })
